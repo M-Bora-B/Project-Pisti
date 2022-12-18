c6cbda12b94516ac42b8ac1c2900c5b1f7304de8
M-Bora-B <mehmetborabocekoglu@gmail.com>
2022-12-18 21:18:18 +0300

    Created Deck Class
    Created Deck Class. Deck consists of 52 cards. Deck Class can shuffle the deck and return it.

---
+++ Deck.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Deck {
+	private Cards[] deck;
+	private int cardnumber = 52;
+	private int currentcard;
+	private Random rd = new Random(System.currentTimeMillis());
+	
+	public Deck {
+		String[] suit = {"♥", "♦", "♣", "♠"}
+		String[] value = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+		deck = new Cards[cardnumber];
+		for(int i=0; i<deck.length; i++) {
+			deck[i] = new Cards(suit[i/13], value[i%13]);
+		}
+	}
+	
+	public void Shuffle() {
+		for(int u=0; u<deck.length; u++) {
+			int random = rd.nextInt(cardnumber);
+			Cards temp = deck[u];
+			deck[u] = deck[random];
+			deck[random] = temp;
+		}
+	}
+	
+	public Cards[] getDeck {
+		return deck;
+	}
+}
