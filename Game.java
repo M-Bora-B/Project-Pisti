6371cd8864719731d5007d28910968442bb422e8
M-Bora-B <mehmetborabocekoglu@gmail.com>
2022-12-22 16:43:20 +0300

    User can cut the deck
    -User can cut the deck by entering an integer value
    -Try catch method asks user to enter an integer value and asks it again if user enters a non integer value

--- Game.java
+++ Game.java
@@ -1,14 +1,38 @@
 import java.util.Scanner;
 
 public class Game {
-	public static void main(String[] args) {
-		int cardnum = 52;
-		Cards[] deck = new Cards[cardnum];
-		Deck temp = new Deck();
-		temp.Shuffle();
-		deck = temp.getDeck();
-		for (int i = 0; i < cardnum; i++) {
-			System.out.println(deck[i].getValue() + " of " + deck[i].getSuit());
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cardnum = 52;
+        Cards[] deck = new Cards[cardnum];
+        Deck temp = new Deck();
+        temp.Shuffle();
+        deck = temp.getDeck();
+        Cards[] cutdeck = new Cards[cardnum];
+		
+		
+        int x = 0;
+        boolean inputValid = false;
+        while (!inputValid) {
+            try {
+                System.out.println("Please enter an integer value for x: ");
+                x = sc.nextInt();
+                inputValid = true;
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a valid integer value.");
+                sc.nextLine(); 
+            }
+        }
+        int hold = 0;
+        for(int a = 0; a < cardnum; a++) {
+            if(a >= cardnum-x) {
+                cutdeck[a] = deck[hold];
+                hold++;
+            } else cutdeck[a] = deck[a+x];
+        }
+		deck = cutdeck;
+		
+		
+		
+    }
 }
